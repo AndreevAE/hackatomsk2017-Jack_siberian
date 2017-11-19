5e5fa97061fa641761f9b918aae81284a50e9271
Alexander Andreev <al101an@mail.ru>
2017-11-19 11:11:37 +0700

    fix returned promises (https://github.com/AndreevAE/hackatomsk2017-Jack_siberian/issues/11)

--- server/src/server/modules/index.js
+++ server/src/server/modules/index.js
@@ -8,54 +8,62 @@ const bankAddress = 'BANK_ADDRESS'
 
 class Wallet {
 
-    constructor(guid, password, options) {
+    constructor(guid, password) {
+        var options = { apiCode: 'walletAPICode' + guid, apiHost: 'http://localhost:3005' }
         this.wallet = new MyWallet(guid, password, options)
         this.bankWallet = new MyWallet(bankGuid, bankPassword, options)
     }
 
     getBalance() {
-        console.log('getBalance')
-        this.wallet.getBalance().then(function (response) { console.log('My balance is %d!', response.balance); })
-    }
-
-    fullHD() {
-        console.log('enable HD')
-        this.wallet.enableHD()
-    }
-
-    getAccounts() {
-        console.log('getAccounts')
-        this.wallet.listAccounts().then(function (response) { console.log(response); })
+        return this.wallet.getBalance()
+                   .then(function (response) { 
+                        let balance = response.balance
+                        return balance
+                    })
+                   .catch(function (error) { 
+                       console.log(error)
+                    })
     }
 
     withdraw(amount) {
-        console.log('withdraw')
-        var bankAddress = this.bankWallet.listAccounts().then(function (response) { console.log('Bank Addresses[0] = %s', response[0].receiveAddress)})
-        var walletAddress = this.wallet.listAccounts().then(function (response) { console.log('Addresses[0] = %s', response[0].receiveAddress)})
+        let bankAddress = ""
+        this.bankWallet.listAccounts()
+                        .then(function (response) { 
+                            bankAddress = response[0].receiveAddress
+                        })
+        console.log(bankAddress)
+        var walletAddress = this.wallet.listAccounts()
+                                       .then(function (response) { 
+                                           console.log('Addresses[0] = %s', response[0].receiveAddress)
+                                        })
         this.wallet.send(bankAddress, amount, {from: walletAddress})
                    .then(function (response) { console.log(response.message)})
                    .catch(function (error) { console.log(error)})
     }
 
     payout(amount, playersCount) {
-        console.log('payout')
-        var bankAddress = this.bankWallet.listAccounts().then(function (response) { console.log('Bank Addresses[0] = %s', response[0].receiveAddress)})
-        var walletAddress = this.wallet.listAccounts().then(function (response) { console.log('Addresses[0] = %s', response[0].receiveAddress)})
+        var bankAddress = this.bankWallet.listAccounts()
+                                         .then(function (response) { 
+                                             console.log('Bank Addresses[0] = %s', response[0].receiveAddress)
+                                            })
+        var walletAddress = this.wallet.listAccounts()
+                                       .then(function (response) { 
+                                           console.log('Addresses[0] = %s', response[0].receiveAddress)
+                                        })
         this.bankWallet.send(walletAddress, amount * playersCount, {from: bankAddress})
                        .then(function (response) { console.log(response)})
                        .catch(function (error) { console.log(error)})
     }
 }
 
-var options = { apiCode: 'myAPICode', apiHost: 'http://localhost:3000' }
-var wallet = new Wallet('28e400a2-8a4d-4d62-b2c2-98fbf5765085', 'TQOnMnT5aqbG7SZ8tRxJ', options)
-wallet.getBalance()
-//wallet.getAccounts()
+var wallet = new Wallet('28e400a2-8a4d-4d62-b2c2-98fbf5765085', 'TQOnMnT5aqbG7SZ8tRxJ')
+let balance1 = wallet.getBalance().then(function (response) {console.log(response.balance)})
+
 wallet.withdraw(100)
 
-var wallet2 = new Wallet(bankGuid, bankPassword, options)
-wallet2.getBalance()
-//wallet2.getAccounts()
+var wallet2 = new Wallet(bankGuid, bankPassword)
+let balance2 = wallet2.getBalance()
+console.log(balance2)
 wallet2.payout(1000, 5)
 
 module.exports = Wallet
